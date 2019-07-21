77d5494ed0a2db51a5a09dfe176f3e5a56d08fde
Walkoss <Walkoss@users.noreply.github.com>
2019-07-21 20:34:18 +0200

    Added user enrollment when created + get user rpc method

--- user_manager/service.py
+++ user_manager/service.py
@@ -2,6 +2,7 @@ import json
 
 from marshmallow import ValidationError
 from nameko.exceptions import BadRequest
+from nameko.rpc import rpc, RpcProxy
 from nameko_sqlalchemy import Database
 from werkzeug.wrappers import Response
 
@@ -10,9 +11,31 @@ from user_manager.models import DeclarativeBase, User, Group, ProfilePicture
 from user_manager.schema import CreateUserSchema
 
 
+class UserManager:
+    name = "user_manager"
+
+    db = Database(DeclarativeBase)
+
+    @rpc
+    def get_user(self, user_id):
+        with self.db.get_session() as sess:
+            user = sess.query(User).filter(User.id == user_id).first()
+            if not user:
+                raise ValueError(f"User id {user_id} doesn't exists")
+            return {
+                "id": user.id,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "expiration_date": user.expiration_date.isoformat(),
+            }
+
+
 class UserManagerService:
     name = "user_manager_http"
+
     db = Database(DeclarativeBase)
+
+    rekognizer = RpcProxy("rekognizer")
 
     @http("POST", "/users", expected_exceptions=(ValidationError, BadRequest))
     def create_user(self, request):
@@ -50,6 +73,11 @@ class UserManagerService:
             for i in profile_pictures:
                 profile_picture = ProfilePicture(picture_url=i, user_id=user.id)
                 sess.add(profile_picture)
+
+        # TODO: change to event pubsub
+        self.rekognizer.enroll_user(
+            user_id=user.id, image_urls=profile_pictures
+        )
 
         # TODO: add profile pictures
         return {
